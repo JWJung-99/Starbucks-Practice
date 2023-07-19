024689b1bd66e69fc0051655186b5ea07ce58985
JWJung-99 <39busy@naver.com>
2023-07-19 12:34:49 +0900

    Start Project

---
+++ js/main.js
@@ -0,0 +1,91 @@
+const searchEl = document.querySelector(".search");
+const searchInputEl = searchEl.querySelector("input");
+
+searchEl.addEventListener("click", function () {
+  searchEl.classList.add("focused");
+  searchInputEl.setAttribute("placeholder", "통합검색");
+});
+
+const fadeEls = document.querySelectorAll(".visual .fade-in");
+
+fadeEls.forEach(function (fadeEl, index) {
+  gsap.to(fadeEl, 1, {
+    delay: (index + 1) * 0.4,
+    opacity: 1,
+  });
+});
+
+new Swiper(".notice-line .swiper", {
+  direction: "vertical",
+  autoplay: true,
+  loop: true,
+});
+
+new Swiper(".promotion .swiper", {
+  slidesPerView: 3,
+  spaceBetween: 10,
+  centeredSlides: true,
+  loop: true,
+  autoplay: {
+    delay: 5000,
+  },
+  pagination: {
+    el: ".promotion .swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    prevEl: ".promotion .swiper-prev",
+    nextEl: ".promotion .swiper-next",
+  },
+});
+
+const promotionEl = document.querySelector(".promotion");
+const promotionToggleButton = document.querySelector(".toggle-promotion");
+
+let isPromotionOpened = false;
+
+promotionToggleButton.addEventListener("click", function () {
+  isPromotionOpened = !isPromotionOpened;
+
+  if (isPromotionOpened) {
+    promotionEl.classList.add("open");
+  } else {
+    promotionEl.classList.remove("open");
+  }
+});
+
+const spyEls = document.querySelectorAll("section.scroll-spy");
+spyEls.forEach(function (spyEl) {
+  new ScrollMagic.Scene({
+    triggerElement: spyEl,
+    triggerHook: 0.8, // viewport 0 ~ 1
+  })
+    .setClassToggle(spyEl, "show")
+    .addTo(new ScrollMagic.Controller());
+});
+
+const thisYear = document.querySelector(".this-year");
+thisYear.textContent = new Date().getFullYear();
+
+const toTopEl = document.querySelector("#to-top");
+
+window.addEventListener(
+  "scroll",
+  _.throttle(function () {
+    if (window.scrollY > 800) {
+      gsap.to(toTopEl, 0.5, {
+        x: -75,
+      });
+    } else {
+      gsap.to(toTopEl, 0.5, {
+        x: 100,
+      });
+    }
+  })
+);
+
+toTopEl.addEventListener("click", function () {
+  gsap.to(window, 0.7, {
+    scrollTo: 0,
+  });
+});
